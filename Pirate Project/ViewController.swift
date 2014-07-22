e23d893ef68051edcdbe33d1d525be75cf6e1578
Bruce Milyko <Wizzard@Bruces-iMac.local>
2014-07-21 21:19:18 -0400

    Initial Commit

---
+++ Pirate Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pirate Project
+//
+//  Created by Bruce Milyko on 7/21/14.
+//  Copyright (c) 2014 Wizzard Works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
